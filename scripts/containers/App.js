58f2570121d30b342c5de6504d45eb28bfba632c
joaquinlarra <joaquinastelarra@gmail.com>
2015-09-28 05:30:17 -0700

    moving app to containers

--- scripts/containers/App.js
+++ scripts/containers/App.js
@@ -1,19 +1,52 @@
 import React, {Component, PropTypes} from 'react';
 import {connect} from 'react-redux';
-import Songs from '../containers/Songs';
+import {fetchSongsIfNeeded} from '../actions/songs';
+
+import Header from '../components/Header';
+import SongPlayer from '../components/SongPlayer';
+import Songs from '../components/Songs';
 
 class App extends Component {
+    componentDidMount () {
+        const {dispatch} = this.props;
+        dispatch(fetchSongsIfNeeded());
+    }
+
+    renderSongPlayer() {
+        const {dispatch, songs} = this.props;
+        if (songs.activeSongIndex === null) {
+            return;
+        }
+
+        return <SongPlayer dispatch={dispatch} song={songs.items[songs.activeSongIndex]} />
+    }
+
     render() {
         return (
-            <Songs />
+            <div>
+                <Header />
+                <div className='container'>
+                    <div className='content'>
+                        <Songs {...this.props} scrollFunc={fetchSongsIfNeeded} />
+                    </div>
+                </div>
+                {this.renderSongPlayer()}
+            </div>
         );
     }
 }
 
-App.PropTypes = {};
+App.propTypes = {
+    dispatch: PropTypes.func.isRequired,
+    songs: PropTypes.object.isRequired,
+};
 
 function mapStateToProps(state) {
-    return {};
+    const { songs } = state;
+    return {
+        songs,
+    };
 }
+
 
 export default connect(mapStateToProps)(App);
