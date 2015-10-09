1bcfca9029d2ce4c74728e9b923e8aab2f2c887f
joaquinlarra <joaquinastelarra@gmail.com>
2015-10-08 17:55:55 -0700

    adding song cards to song detail view

--- scripts/components/SongsCard.js
+++ scripts/components/SongsCard.js
@@ -1,15 +1,41 @@
 import React, {Component, PropTypes} from 'react';
+import SongDetails from '../components/SongDetails';
+import {getImageUrl} from '../helpers/SongsHelper';
 
 class SongsCard extends Component {
     render() {
+        const {dispatch, isActive, playSong, song} = this.props;
+        const image = getImageUrl(song.artwork_url);
+
         return (
-            <div className='card song-card'></div>
+            <div className={'card songs-card' + (isActive ? ' active' : '')}>
+                <div
+                    className='songs-card-image'
+                    onClick={playSong}
+                    style={{backgroundImage: `url(${image})`}}>
+                    <div className='songs-card-playing'>
+                        <i className={'songs-card-playing-icon icon ' + (isActive ? 'ion-radio-waves' : 'ion-ios-play')}></i>
+                    </div>
+                </div>
+                <div className='songs-card-user'>
+                    <img
+                        className='songs-card-user-image'
+                        src={song.user.avatar_url} />
+                    <SongDetails
+                        dispatch={dispatch}
+                        songId={song.id}
+                        title={song.title}
+                        username={song.user.username} />
+                </div>
+            </div>
         );
     }
 }
 
 SongsCard.propTypes = {
-    song: PropTypes.object.isRequired,
+    isActive: PropTypes.bool.isRequired,
+    playSong: PropTypes.func.isRequired,
+    song: PropTypes.object.isRequired
 };
 
 export default SongsCard;
