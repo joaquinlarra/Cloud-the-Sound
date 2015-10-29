0c76e1dcf11e253e70e262193e7de72379d2dbec
joaquinlarra <joaquinastelarra@gmail.com>
2015-10-28 22:13:56 -0700

    receiving related auth data

---
+++ scripts/actions/authed.js
@@ -0,0 +1,104 @@
+import {arrayOf, normalize} from 'normalizr';
+import SC from 'soundcloud';
+import {navigateTo} from '../actions/navigator';
+import {fetchSongs, receiveSongs} from '../actions/playlists';
+import * as types from '../constants/ActionTypes';
+import {CLIENT_ID} from '../constants/Config';
+import {playlistSchema, songSchema} from '../constants/Schemas';
+
+function authUser(accessToken) {
+    return dispatch => {
+        dispatch(receiveAccessToken(accessToken));
+        dispatch(fetchAuthedUser(accessToken));
+        dispatch(fetchStream(accessToken));
+        dispatch(navigateTo({path: ['me', 'stream']}));
+    };
+}
+
+function fetchAuthedUser(accessToken) {
+    return dispatch => {
+        return fetch(`http://api.soundcloud.com/me?oauth_token=${accessToken}`)
+            .then(response => response.json())
+            .then(json => dispatch(receiveAuthedUserPre(accessToken, json)))
+            .catch(error => {throw error});
+    };
+}
+
+function fetchLikes(accessToken) {
+    return dispatch => {
+        return fetch(`http://api.soundcloud.com/me/favorites?oauth_token=${accessToken}`)
+            .then(response => response.json())
+            .then(json => {
+                const songs = json.filter(song => song.streamable);
+                const normalized = normalize(songs, arrayOf(songSchema));
+                dispatch(receiveSongs(normalized.entities, normalized.result, null, 'likes'));
+            })
+            .catch(error => {throw error});
+    };
+}
+
+function fetchPlaylists(accessToken) {
+    return dispatch => {
+        return fetch(`http://api.soundcloud.com/me/playlists?oauth_token=${accessToken}`)
+            .then(response => response.json())
+            .then(json => {
+                const normalized = normalize(json, arrayOf(playlistSchema));
+                dispatch(receivePlaylists(normalized.result, normalized.entities));
+                normalized.result.forEach(playlistId => {
+                    const playlist = normalized.entities[playlistId];
+                    dispatch(receiveSongs({}, playlist.tracks, null, playlist.name));
+                });
+            })
+            .catch(error => { throw error; });
+    };
+}
+
+function fetchStream(accessToken) {
+    return dispatch => {
+        dispatch(fetchSongs(`http://api.soundcloud.com/me/activities/tracks/affiliated?limit=50&oauth_token=${accessToken}`, 'stream'));
+    };
+}
+
+export function loginUser() {
+    return dispatch => {
+        SC.initialize({
+            client_id: CLIENT_ID,
+            redirect_uri: `http://${window.location.host}/api/callback`
+        });
+
+        SC.connect().then(authObj => dispatch(authUser(authObj.oauth_token)))
+        .catch(error => {
+            throw error;
+        });
+    };
+}
+
+function receiveAccessToken(accessToken) {
+    return {
+        type: types.RECEIVE_ACCESS_TOKEN,
+        accessToken
+    };
+}
+
+function receiveAuthedUserPre(accessToken, user) {
+    return dispatch => {
+        dispatch(receiveAuthedUser(user));
+        dispatch(fetchLikes(accessToken));
+        dispatch(fetchPlaylists(accessToken));
+    };
+}
+
+function receiveAuthedPlaylists(playlists, entities) {
+    return {
+        type: types.RECEIVE_AUTHED_PLAYLISTS,
+        entities,
+        playlists
+    };
+}
+
+function receiveAuthedUser(user) {
+    return {
+        type: types.RECEIVE_AUTHED_USER,
+        user
+    };
+}
---
+++ scripts/reducers/authed.js
@@ -0,0 +1,26 @@
+import * as types from '../constants/ActionTypes';
+
+const initialState = {
+    accessToken: null,
+    playlists: [],
+    user: null
+};
+
+export default function authed(state = initialState, action) {
+    switch(action.type) {
+    case types.RECEIVE_ACCESS_TOKEN:
+        return Object.assign({}, state, {
+            accessToken: action.accessToken
+        });
+    case types.RECEIVE_AUTH_USER:
+        return Object.assign({}, state, {
+            user: action.user
+        });
+    case types.RECEIVE_AUTH_PLAYLISTS:
+        return Object.assign({}, state, {
+            playlists: action.playlists
+        });
+    default:
+        return state;
+    }
+}
