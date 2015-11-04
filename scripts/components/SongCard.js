d8e2b791b6e863360d13fce34bb4893f990ce521
joaquinlarra <joaquinastelarra@gmail.com>
2015-11-03 20:13:43 -0800

    changing SongsCard to SongCard

--- scripts/components/SongCard.js
+++ scripts/components/SongCard.js
@@ -1,83 +1,156 @@
 import React, {Component, PropTypes} from 'react';
-import Link from '../components/Link';
-import SongHeartCount from '../components/SongHeartCount';
-import Waveform from '../components/Waveform';
+import {playSong} from '../actions/player';
+import {fetchSongsIfNeeded} from '../actions/playlists';
+import InfiniteScrollify from '../components/InfiniteScrollify';
+import SongCard from '../components/SongCard';
+import Spinner from '../components/Spinner';
 
-import {IMAGE_SIZES} from '../constants/SongConstants';
+class SongCards extends Component {
+    constructor(props) {
+        super(props);
+        this.getScrollState = this.getScrollState.bind(this);
+        this.onScroll = this.onScroll.bind(this);
+        const {playlist, playlists} = props;
+        const items = playlist in playlists ? playlists[playlist].items : [];
+        this.state = {
+            end: items.length,
+            paddingBottom: 0,
+            paddingTop: 0,
+            start: 0
+        };
+    }
 
-import {addCommas} from '../utils/FormatUtils';
-import {getImageUrl} from '../utils/SongUtils';
+    componentDidMount() {
+        window.addEventListener('scroll', this.onScroll, false);
+    }
 
-class SongCard extends Component {
+    componentWillReceiveProps(nextProps) {
+        const {end, paddingBottom, paddingTop, start} = this.getScrollState(nextProps);
+        if (paddingTop !== this.state.paddingTop
+        || paddingBottom !== this.state.paddingBottom
+        || start !== this.state.start
+        || end !== this.state.end) {
+            this.setState({
+                end: end,
+                paddingBottom: paddingBottom,
+                paddingTop: paddingTop,
+                start: start
+            });
+        }
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.onScroll, false);
+    }
+
+    getScrollState(props) {
+        const {height, playlists, playlist} = props;
+        const items = playlist in playlists ? playlists[playlist].items : [];
+
+        const MARGIN_TOP = 20;
+        const ROW_HEIGHT = 132;
+        const ITEMS_PER_ROW = 5;
+        const scrollY = window.scrollY;
+        let paddingTop = 0;
+        let paddingBottom = 0;
+        let start = 0;
+        let end = items.length;
+
+        if ((scrollY - ((ROW_HEIGHT * 3) + (MARGIN_TOP * 2))) > 0) {
+            const rowsToPad = Math.floor((scrollY - ((ROW_HEIGHT * 2) + (MARGIN_TOP))) / (ROW_HEIGHT + MARGIN_TOP));
+            paddingTop = rowsToPad * (ROW_HEIGHT + MARGIN_TOP);
+            start = rowsToPad * ITEMS_PER_ROW;
+        }
+
+        const rowsOnScreen = Math.ceil(height / (ROW_HEIGHT + MARGIN_TOP));
+        const itemsToShow = (rowsOnScreen + 5) * ITEMS_PER_ROW;
+        if (items.length > (start + itemsToShow)) {
+            end = start + itemsToShow;
+            const rowsToPad = Math.ceil((items.length - end) / ITEMS_PER_ROW);
+            paddingBottom = rowsToPad * (ROW_HEIGHT + MARGIN_TOP);
+        }
+
+        return {
+            end,
+            paddingBottom,
+            paddingTop,
+            start
+        };
+    }
+
+    onScroll() {
+        const {end, paddingBottom, paddingTop, start} = this.getScrollState(this.props);
+        if (paddingTop !== this.state.paddingTop
+        || paddingBottom !== this.state.paddingBottom
+        || end !== this.state.end
+        || start !== this.state.start) {
+            this.setState({
+                end: end,
+                paddingBottom: paddingBottom,
+                paddingTop: paddingTop,
+                start: start
+            });
+        }
+    }
+
+    playSong(i) {
+        const {playlist, dispatch} = this.props;
+        dispatch(playSong(playlist, i));
+    }
+
+    renderSongs(start, end) {
+        const chunk = 5;
+        const {authed, dispatch, playlist, playlists, playingSongId, songs, users} = this.props;
+        const items = playlist in playlists ? playlists[playlist].items : [];
+        let result = [];
+        for (let i = start; i < end; i += chunk) {
+            let songCards = items.slice(i, i + chunk).map((songId, j) => {
+                const song = songs[songId];
+                const user = users[song.user_id];
+                const index = i + j;
+                return (
+                    <div className='col-1-5' key={index + '-' + song.id}>
+                        <SongCard
+                            authed={authed}
+                            dispatch={dispatch}
+                            isActive={song.id === playingSongId}
+                            playSong={this.playSong.bind(this, index)}
+                            scrollFunc={fetchSongsIfNeeded.bind(null, playlist)}
+                            song={song}
+                            user={user} />
+                    </div>
+                );
+            }, this);
+
+            if (songCards.length < chunk) {
+                for (let j = 0; j < chunk - songCards.length + 1; j++) {
+                    songCards.push(<div className='col-1-5' key={'song-placeholder-' + (i + j)}></div>);
+                }
+            }
+
+            result.push(
+                <div className='songs-row grid' key={'songs-row-' + i}>{songCards}</div>
+            );
+        }
+
+        return result;
+    }
+
     render() {
-        const {authed, dispatch, isActive, player, playSong, song, user} = this.props;
-        const image = getImageUrl(song.artwork_url, IMAGE_SIZES.LARGE);
+        const {playlist, playlists} = this.props;
+        const {end, paddingBottom, paddingTop, start} = this.state;
+        const isFetching = playlist in playlists ? playlists[playlist].isFetching : false;
+
 
         return (
-            <div className={'song-card' + (isActive ? ' active' : '')}>
-                <div className='song-card-detail'>
-                    <div
-                        className='song-card-image'
-                        onClick={playSong}
-                        style={{backgroundImage: `url(${image})`}}>
-                        <div className='songs-card-playing'>
-                            <i className={'songs-card-playing-icon icon ' + (isActive ? 'ion-radio-waves' : 'ion-ios-play')}></i>
-                        </div>
-                    </div>
-                    <div className='song-card-info'>
-                        <Link
-                            className='song-card-title'
-                            dispatch={dispatch}
-                            route={{path: ['songs', song.id]}}>
-                            {song.title}
-                        </Link>
-                        <div className='song-card-info-extra'>
-                            <div className='song-card-user'>
-                                <div
-                                    className='song-card-user-image'
-                                    style={{backgroundImage: `url(${getImageUrl(user.avatar_url)})`}}>
-                                </div>
-                                <Link
-                                    className='song-card-username'
-                                    dispatch={dispatch}
-                                    route={{path: ['users', song.user_id]}}>
-                                    {user.username}
-                                </Link>
-                            </div>
-                            <div className='song-card-stats'>
-                                <SongHeartCount
-                                    authed={authed}
-                                    count={song.favoritings_count}
-                                    dispatch={dispatch}
-                                    songId={song.id} />
-                                <div className='song-card-stat'>
-                                    <i className='icon ion-play'></i>
-                                    <span>{addCommas(song.playback_count)}</span>
-                                </div>
-                                <div className='song-card-stat'>
-                                    <i className='icon ion-chatbubble'></i>
-                                    <span>{addCommas(song.comment_count)}</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className='song-card-waveform'>
-                    <Waveform
-                        currentTime={player.currentTime}
-                        dispatch={dispatch}
-                        duration={song.duration}
-                        isActive={isActive}
-                        playSong={playSong}
-                        waveformUrl={song.waveform_url.replace('https', 'http')} />
-                </div>
+            <div className='content'>
+                <div className='padder' style={{height: paddingTop}}></div>
+                {this.renderSongs(start, end)}
+                <div className='padder' style={{height: paddingBottom}}></div>
+                {isFetching ? <Spinner /> : null}
             </div>
         );
     }
 }
 
-SongCard.propTypes = {
-    song: PropTypes.object.isRequired
-};
-
-export default SongCard;
+export default InfiniteScrollify(SongCards);
